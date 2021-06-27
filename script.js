2406c82d03e2f67ed45c733aaf2c58961e983db0
Bruno Richard <bruno683@outlook.fr>
2021-06-27 22:10:03 +0200

    Adding script from blotter

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-let text = new Blotter.Text('Ants !', {
+
+const text = new Blotter.Text('Ants !', {
   family: 'serif',
   size: '150',
   fill: 'white',
 })
 
-const material = new Blotter.fliesMaterial();
-material.uniforms.uPointCellWidth.value = 0.01;
-material.uniforms.uPointRadius.value = 0.8;
+const material = new Blotter.FliesMaterial();
+material.uniforms.uPointCellWidth.value= 0.01;
+material.uniforms.uPointRadius.value = .8;
 material.uniforms.uSpeed.value = 5;
 
 
-const blotter = new Blotter(material,{
-  texts : text,
+const blotter = new Blotter(material, {
+  texts: text,
 })
 
 const scope = blotter.forText(text);
